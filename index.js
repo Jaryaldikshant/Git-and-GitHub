bb91836c42d611ae03b9e756b410d25a165d94f9
Dikshant Jaryal <dikshant.jaryal08@gmail.com>
2024-10-14 10:35:23 +0530

    Added above

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const age = 24;
+const name = "Dikshant Jaryal";
+const dob = "01/08/2004";
+
+function sayName(){
+    console.log("My name is: "+ name);
+}
+
+function isAbove(){
+    return age >=18;
+}
+
