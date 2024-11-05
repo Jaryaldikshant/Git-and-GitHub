ec8a0fb1e9b74a06e0229cd8dfceca21ba5303df
Dikshant Jaryal <dikshant.jaryal08@gmail.com>
2024-11-05 11:19:26 +0530

    git

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ function sayHello(){
     console.log("Saying Hello....");
 }
 
-  
+function sayBye() {  
+    console.log("Saying Bye.....");
+}  
+
+function greeting(){
+    console.log("Good Morning");
+}
