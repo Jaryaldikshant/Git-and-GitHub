65fd4bdefb0819bb820c276bf3604692d7c0650b
Dikshant Jaryal <dikshant.jaryal08@gmail.com>
2024-11-06 10:01:28 +0530

    feat b added

--- Util.js
+++ Util.js
@@ -6,9 +6,6 @@ function remove(a, num2){
     return 0;
 }
 
-
-function newFeatureAdd() {
-    console.log("This is a new feature");
-    console.log("This is a new feature A");
-
-  }
+function newFeatureB() {
+    console.log("This is a new Feature B");
+}
